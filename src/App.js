90403329deaa69d7897810b06bde7ca3a9684aae
filipenickel <filipenickel@gmail.com>
2018-06-02 22:41:30 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import React, { Component } from 'react'
-import './index.css'
-import { Route, Switch } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import CategoriasList from './component/CategoriasList'
-import PostsTable from './component/PostsTable'
-import CriarPostagem from './component/CriarPostagem'
-import EditarPostagem from './component/EditarPostagem'
-import EditarComentario from './component/EditarComentario'
-import Post from './component/Post'
-
+import React, { Component } from "react";
+import "./index.css";
+import { Route, Switch,browserHistory,Router } from "react-router-dom";
+import CategoriasList from "./component/CategoriasList";
+import PostsTable from "./component/PostsTable";
+import CriarPostagem from "./component/CriarPostagem";
+import EditarPostagem from "./component/EditarPostagem";
+import EditarComentario from "./component/EditarComentario";
+import Post from "./component/Post";
+import Erro from "./component/Erro";
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <header>
-          <h1 style={{textAlign: 'center'}}><Link to="/">Comentarios</Link></h1>
+          <h1 style={{ textAlign: "center" }}>Comentarios</h1>
         </header>
-        <Route exact path='/' render={(props) => (
-          <main>
-            <CategoriasList {...props}/>
-            <PostsTable {...props}/>
-          </main>
-        )}/>
-        <Switch>
-          <Route exact path='/:categoria' render={(props) => (
-            <main>
-              <CategoriasList {...props}/>
-              <PostsTable {...props}/>
-            </main>
-          )}/>
-          <Route exact path='/postagens/criar' component={CriarPostagem}/>
-          <Route exact path='/:categoria/:id' component={Post}/>
-          <Route exact path='/postagens/:id/editar' component={EditarPostagem}/>
-          <Route exact path='/comentarios/:id/editar' component={EditarComentario}/>
-        </Switch>
+        <main>
+          <CategoriasList />
+          
+          <Switch>
+            <Route exact path="/" render={props => <PostsTable {...props} />} />
+            <Route
+              exact
+              path="/:categoria"
+              render={props => <PostsTable {...props}
+               />
+            }
+            />
+            <Route exact path="/postagens/criar" component={CriarPostagem} />
+            <Route exact path="/:categoria/:id" component={Post} />
+            <Route
+              exact
+              path="/postagens/:id/editar"
+              component={EditarPostagem}
+            />
+            <Route
+              exact
+              path="/comentarios/:id/editar"
+              component={EditarComentario}
+            />
+            <Route path='*' component={Erro} />
+            
+          </Switch> 
+          
+        </main>
       </div>
     );
   }
